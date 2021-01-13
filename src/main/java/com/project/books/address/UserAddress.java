6ca731c28d3eab7464dfaf519177afc01339efea
marika9393 <marika-piotrowska@wp.pl>
2021-01-13 20:54:16 +0100

    add userAddress

---
+++ src/main/java/com/project/books/address/UserAddress.java
@@ -0,0 +1,30 @@
+package com.project.books.address;
+
+
+import com.project.books.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long addressId;
+
+    private String region;
+    private String city;
+    private String street;
+    private String postCode;
+
+    @OneToMany(mappedBy = "userAddress")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<User> user;
+
+}
